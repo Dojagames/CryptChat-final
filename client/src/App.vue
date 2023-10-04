5e1083877de882e5837a10ba99226c6ad3404167
jonx <dev.jonx.eu@gmail.com>
2023-10-04 20:50:03 +0200

    backend

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,41 +1,186 @@
 <script>
-    import {io} from 'socket.io-client'
-    const socket = io('localhost:3007');
+  import {io} from 'socket.io-client'
+  const socket = io('localhost:3007');
+
+export default {
+
+    data(){
+        return {
+            view: "login",
+
+            loginInput: "",
+            loginPassword: "",
+            loginStayLoggedIn: true,
+            registerPassword: "",
+            registerPassword2: "",
+            registerUser: "",
 
 
-  export default {
+            refreshToken: undefined,
 
-      data(){
-          return {
-              
-          }
-      },
-      components: {
-          
-      },
-      props: {
-          
-      },
-      methods: {
-          
-      },
-      created() {
-          
-      },
-      mounted(){
-          
-      },
-      watch: {
-          
-      },
 
-  }
+            msgto: "",
+            msg: "",
+        }
+    },
+    components: {
+        
+    },
+    props: {
+        
+    },
+    methods: {
+        Login(user, pw, stayLoggedin){
+            socket.emit("login", {username: user, password: pw, stayLogged: stayLoggedin});
+        },
+        Register(user, pw1, pw2){
+            //reset warnings
+            if(pw1 != pw2){
+                //show pw must match
+                return;
+            }
+
+            // if(p1.length < 11){
+            //     //show pw must be at least 12 characters long
+            //     return;
+            // }
+            socket.emit("register", {"username": user, "password": pw1})
+        },
+
+        GetToken(){
+            socket.emit("getToken", (refreshToken))
+        },
+
+        logOut(){
+            socket.emit("logout");
+            // localStorage.removeItem("token");
+            // sessionStorage.removeItem("tempToken");
+            // this.view = "login";
+        },
+
+        changeView(_view){
+            this.view = _view;
+        },
+
+        sendMsg(){
+            socket.emit("sendMsg", ({to: this.msgto, msg: this.msg}));
+        },
+        
+    },
+    created() {
+        socket.on("cantLogin", () => {
+           alert("noUser")
+        });
+
+        socket.on("cantRegister", (_cause) => {
+           alert("test");
+            //show cant register
+        });
+
+        socket.on("loggedIn", (token) => {
+            sessionStorage.setItem("tempToken", token);
+            this.view = "main";
+        });
+
+        socket.on("alreadyLoggedIn", () => {
+            alert("alreadyLoggedIn");
+        });
+
+        socket.on("registered", () => {
+            this.view = "main";
+        });
+
+        socket.on("deleteToken", () => {
+            localStorage.removeItem("token");
+            //show that you have been locked out
+        });
+
+        socket.on("tempTokenExpired", () => {
+            this.refreshToken = localStorage.getItem("token");
+            if(this.refreshToken){
+                socket.emit("getTempToken", (this.refreshToken));
+            }
+        });
+
+        socket.on("getRefreshToken", (token) =>{
+            this.refreshToken = token;
+            localStorage.setItem("token", token);
+        });
+
+        socket.on("getMsg", (msg) => {
+            alert(msg.from + " : " + msg.msg);
+        });
+
+    },
+    mounted(){
+        const tempToken = sessionStorage.getItem("tempToken");
+        if(!tempToken){
+            this.refreshToken = localStorage.getItem("token");
+            if(this.refreshToken) {
+                socket.emit("getTempToken", (this.refreshToken));
+            }
+        } else {
+            socket.emit("verifyToken", (tempToken));
+        }
+
+       
+        
+    },
+    watch: {
+        
+    },
+
+}
 </script>
 
 <template>
     
+    <div id="login" v-if="view === 'login'">
+        <h3>Login</h3>
+        <input type="text" placeholder="username" v-model="loginInput" style="background-color: transparent;"><br>
+        <input type="password" placeholder="password" v-model="loginPassword" style="background-color: transparent;"><br>
+        <button @click="Login(loginInput, loginPassword, false)">login</button>
+
+        <br><br>
+        <button @click="changeView('register')">Register?</button>
+    </div>
+
+    <div id="register" v-if="view === 'register'">
+        <h3>Register</h3>
+        <input type="text" placeholder="username" v-model="registerUser" style="background-color: transparent;"><br><br>
+
+        <input type="password" placeholder="password" v-model="registerPassword" style="background-color: transparent;"><br>
+        <input type="password" placeholder="password" v-model="registerPassword2" style="background-color: transparent;"><br>
+        <button @click="Register(registerUser, registerPassword, registerPassword2 )">Register</button>
+
+        <br><br>
+        <button @click="changeView('login')">Login?</button>
+    </div>
+
+    <div id="main" v-if="view === 'main'">
+        <button @click="logOut()">log out</button>
+        <div id="window" style="position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); text-align: center;">
+            <input type="text" style="background-color: transparent;" v-model="msgto" placeholder="send to"><br>
+            <input type="text" style="background-color: transparent;" v-model="msg" placeholder="msg"><br>
+            <button @click="sendMsg()">send</button>
+        </div>
+        
+    </div>
+
 </template>
 
 <style scoped>
-    
+    #login{
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
+
+        width: 300px;
+        height: 400px;
+
+        background-color: rgba(255, 0, 0, 0.178);
+
+        text-align: center;
+    }
 </style>
--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
+const {MongoClient} = require('mongodb');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 const server = require('http').createServer();
 const io = require('socket.io')(server, {
     cors: {
@@ -7,12 +12,160 @@ const io = require('socket.io')(server, {
 });
 
 
+var msgCollection;
+var userCollection;
+var tokenCollection;
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
+
 io.on('connection', (socket)=> {
     socket.emit("test", "testmsg");
+
+
+    //acounts
+    socket.on("getTempToken", async (_token) => {
+        jwt.verify(_token, process.env.REFRESH_TOKEN_SECRET, async (err, user) =>{
+            if (err) socket.emit("deleteToken"); 
+
+            if(Users.some(e => e.name == user.user)){
+                socket.emit("alreadyLoggedIn");
+                return;
+            }
+            
+            let _token = await tokenCollection.findOne({ "_id": user.user });
+            if(!_token) {
+                console.log("noDBentryFound");
+                socket.emit("deleteToken");
+                return;
+            }
+
+            const jwtToken = jwt.sign({user: user.user}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "30m"}); 
+            Users.push({name: user.user, id: socket.id});
+            
+            socket.emit("loggedIn", (jwtToken));
+        });
+        //check token
+        //if valid -> login 
+        // else send invalid token + delete token on client and send to login page with "you have been locked out"
+    });
+
+    socket.on("verifyToken", async ( _token) => {
+        jwt.verify(_token, process.env.ACCESS_TOKEN_SECRET, async (err, user) =>{
+            if (err) {
+               socket.emit("tempTokenExpired");
+               return;
+            }
+
+            let userToken = await tokenCollection.findOne({ "_id": user.user });
+            if(!userToken) {
+                console.log("noDBentryFound");
+                socket.emit("deleteToken");
+                return;
+            }
+            
+            if(Users.some(e => e.name == user.user)){
+                socket.emit("alreadyLoggedIn");
+                return;
+            }
+            
+            Users.push({name: user.user, id: socket.id});
+            socket.emit("loggedIn", (_token));
+        });
+    });
+
+    socket.on("login", async (_data) => {
+
+        if(Users.some(e => e.name == _data.username)){
+            socket.emit("alreadyLoggedIn");
+            return;
+        }
+        
+        let result = await userCollection.findOne({ "_id": _data.username });
+        if(!result) {
+            socket.emit("cantLogin");
+            return;
+        }
+
+        
+        try{
+            if (await bcrypt.compare(_data.password, result.pw)){
+
+                let _token = await tokenCollection.findOne({ "_id": _data.username });
+                if(!_token){
+                    await tokenCollection.insertOne({ "_id": _data.username})
+                }
+
+                const jwtRefreshToken = jwt.sign({user: _data.username}, process.env.REFRESH_TOKEN_SECRET);
+                const jwtTempToken = jwt.sign({user: _data.username}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "30m"});  
+                Users.push({name: _data.username, id: socket.id});
+
+                socket.emit("getRefreshToken", (jwtRefreshToken));
+                socket.emit("loggedIn", (jwtTempToken));
+            } else {
+                socket.emit("cantLogin"); 
+            }
+        }catch{
+            console.log("errorCom")
+        } 
+    });
+
+    socket.on("register", async (_data) => {
+        let result = await userCollection.findOne({ "_id": _data.username });
+
+        if(result) {
+            socket.emit("cantRegister", ("user already exists"));
+            return;
+        } 
+
+        try {
+            const _pw = await bcrypt.hash(_data.password, 10);
+            await userCollection.insertOne({ "_id": _data.username, "pw": _pw});
+            socket.emit("registered");
+            Users.push({name: _data.username, id: socket.id});
+        } catch {
+            socket.emit("cantRegister", ("server error"));
+        }
+    });
+
+    socket.on("logout", async () =>{
+        // console.log(Users.filter(e => e.id == socket.id)[0].name);
+        const name = Users.filter(e => e.id == socket.id)[0].name;
+        await tokenCollection.deleteOne({ "_id": name});
+    })
+
+    socket.on("disconnecting", () => {
+        Users = Users.filter(e => e.id != socket.id);
+    });
+
+    //msgs
+    socket.on("sendMsg", (_data) =>{
+        console.log(Users.some(e => e.name == _data.to));
+        if(Users.some(e => e.name == _data.to)){
+            console.log("test");
+            const _id = Users.filter(e => e.name == _data.to)[0].id;
+            io.to(_id).emit("getMsg", ({from: "user", msg: _data.msg}));
+        }
+    })
 });
 
 
-let adRooms = [];
+var Users = [];
 
 
-server.listen(3007);
+
+
+
+
+server.listen(3007, async () => {
+    try {
+        await client.connect();
+        userCollection = client.db("CC-local").collection("Users");
+        msgCollection = client.db("CC-local").collection("msgs");
+        tokenCollection = client.db("CC-local").collection("tokens");
+        console.log("DB connected...");
+    } catch (e){
+        console.error(e);
+    }
+});
