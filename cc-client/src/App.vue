f86f443afc03f5d9c18247b3ebb204832ea81273
jonx <dev.jonx.eu@gmail.com>
2024-10-26 00:36:13 +0200

    added unversioned files

---
+++ cc-client/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import { RouterLink, RouterView, useRouter } from 'vue-router';
+  import Background  from "@/components/background.vue";
+  import {onMounted} from "vue";
+  import {useSession} from '@/stores/sessionStore.js';
+
+  const session = useSession();
+  //startup logic goes here
+  let loggedIn = session.session; //should check regularly if still logged in.
+  console.log(session.session);
+
+
+  const router = useRouter();
+
+  if(!loggedIn){
+    router.push('/login');
+  }
+
+
+
+</script>
+
+<template>
+  <Background/>
+  <!--<RouterView /> -->
+</template>
+
+<style scoped>
+
+</style>
---
+++ cc-client/src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/home.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/login.vue')
+    }
+  ]
+})
+
+export default router
