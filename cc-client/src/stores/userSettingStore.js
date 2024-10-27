0da1a8054a6aa6db3c1b31e2f8c928bb1eb83b81
jonx <dev.jonx.eu@gmail.com>
2024-10-28 00:25:20 +0100

    userSettingStore init

---
+++ cc-client/src/stores/userSettingStore.js
@@ -0,0 +1,30 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useSettingsStore = defineStore("settings", () => {
+  const settings = ref({});
+  settings.value = {
+    biometricLock: false,
+    profilePicture: "",
+    notification: {
+      tone: "default",
+      vibrate: "default",
+    },
+    theme: "default",
+    font: "default",
+    backup: {
+      passwordProtected: false,
+      storageLocation: "local",
+      autoBackup: false,
+      frequency: "30d",
+    },
+    privacy: {
+      lastOnline: false,
+      Online: false,
+      typingIndicator: false,
+      profilePicture: "nobody",
+      bio: "nobody",
+      blockedContacts: [],
+    }
+  }
+})
