a84c687e045add6f60925ba2ec0400b6331e6dce
jonx <dev.jonx.eu@gmail.com>
2024-10-28 02:00:41 +0100

    begin of design

--- cc-client/src/components/chat-preview.vue
+++ cc-client/src/components/chat-preview.vue
@@ -15,11 +15,37 @@ function goToChat(){
 </script>
 
 <template>
-  <div @click="goToChat">
-    {{props.chat}}
+  <div @click="goToChat" class="preview-container">
+    <img class="preview-img" src="https://cdn.britannica.com/98/214598-050-9879F2FA/giant-sequoia-tree-Sequoia-National-Park-California.jpg" alt="pb">
+    <div class="preview-name">{{props.chat}}</div>
+    <div class="preview-lastMsg"></div>
+    <div class="timeStamp"></div>
+    <div class="pinned"></div>
+
   </div>
 </template>
 
 <style scoped>
+  .preview-container {
+    position: relative;
+    width: 98vw;
+    height: 6vh;
+    background-color: rgba(255, 255, 255, 0.2);
+    backdrop-filter: blur(4px);
+    margin: 1vw;
 
+    border-radius: 1vw;
+  }
+
+  .preview-img {
+    height: 80%;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    position: relative;
+
+    top:50%;
+    left: 1vw;
+    transform: translateY(-50%);
+    object-fit: cover;
+  }
 </style>
--- cc-client/src/components/home-header.vue
+++ cc-client/src/components/home-header.vue
@@ -5,25 +5,39 @@
 <template>
   <div id="home-header">
     <h1>CryptChat</h1>
+    <div id="header-settings"></div>
   </div>
 </template>
 
 <style scoped>
   #home-header {
-    position: relative;
+    position: sticky;
     top: 0;
     background-color: var(--secondary-background-color);
     width: 100%;
-    height: 12vh;
-    min-height: 12vh;
+    height: 8vh;
+    min-height: 8vh;
   }
 
   h1{
+    position: relative;
     text-align: center;
     margin: 0;
-    position: relative;
-    top: 25%;
-
+    top: 18%;
+    left: 50%;
+    transform: translateX(-50%);
+    width: fit-content;
     font-size: 2rem;
   }
+
+  #header-settings{
+    position: absolute;
+    right: 1%;
+    top:50%;
+    transform: translateY(-50%);
+    height: 50%;
+    aspect-ratio: 1;
+    background-color: red;
+  }
+
 </style>
